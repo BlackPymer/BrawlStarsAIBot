fcc9532923bdf98d86cca1bd22ff83e691320ca8
BlackPymer <g39palchevski@gmail.com>
2026-07-15 17:36:08 +0300

    update to use 100 pixels

--- train_hp.py
+++ train_hp.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=11):
         super().__init__()
         self.cnn = nn.Sequential(
             nn.Conv2d(3, 64, 3, padding=1),
@@ -24,6 +24,16 @@ class Net(nn.Module):
             nn.Conv2d(512, 512, 3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d((2, 1)),
+            nn.MaxPool2d((2, 1)),
         )
         self.rnn = nn.LSTM(512, 256, bidirectional=True, batch_first=True)
-        
+        self.head = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        x = self.cnn(x)
+        b, c, h, w = x.shape
+        x = x.reshape(b, c * h, w)
+        x = x.permute(0, 2, 1)
+        x, _ = self.rnn(x)
+        x = self.head(x)
+        return x.permute(1, 0, 2)
