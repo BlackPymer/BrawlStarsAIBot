a292a94948529f49df4a5904a9e5168ac288716d
BlackPymer <g39palchevski@gmail.com>
2026-07-25 23:04:18 +0300

    created architecture v1 and connected it to script

--- src/network/network_engine.py
+++ src/network/network_engine.py
@@ -1,5 +1,40 @@
+import torch
+
+from network.exceptions import PlayerNotFoundException
+from objects_detection.object_detector import CLASS_NAMES
+from player_network import PlayerNetwork, INPUT_HEIGHT, INPUT_WIDTH
+
+MAX_HP_VALUE = 20000
+
+
 class NetworkEngine:
     def __init__(self):
-        pass
-    def make_action(self, objects:list, hps:list):
-        pass
+        self.player = PlayerNetwork()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.player = self.player.to(self.device)
+
+    def make_action(self, objects: list, hps: list, ult: bool, frame_width: int, frame_height: int):
+        map_inp = torch.zeros(size=(1, 8, INPUT_HEIGHT, INPUT_WIDTH), device=self.device)
+        hp_and_player_count = 0
+        player_hp_inp = 0
+        ult_inp = torch.tensor([[1.0 if ult else 0.0]], device=self.device)
+        for obj in objects:
+            for box in obj.boxes:
+                cls_id = int(box.cls[0])
+                x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+                nx1 = round(x1 / frame_width * INPUT_WIDTH)
+                nx2 = round(x2 / frame_width * INPUT_WIDTH)
+                ny1 = round(y1 / frame_height * INPUT_HEIGHT)
+                ny2 = round(y2 / frame_height * INPUT_HEIGHT)
+                if CLASS_NAMES[cls_id] in ("player", "enemy"):
+
+                    map_inp[0, cls_id, ny1:ny2, nx1:nx2] = hps[hp_and_player_count] / MAX_HP_VALUE
+                    if CLASS_NAMES[cls_id] == "player":
+                        player_hp_inp = torch.tensor([[hps[hp_and_player_count] / MAX_HP_VALUE]], device=self.device)
+                    hp_and_player_count += 1
+
+                else:
+                    map_inp[0, cls_id, ny1:ny2, nx1:nx2] = 1
+        if isinstance(player_hp_inp, int):
+            raise PlayerNotFoundException
+        return self.player(map_inp, player_hp_inp, ult_inp)
---
+++ src/network/player_network.py
@@ -0,0 +1,64 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torch import Tensor
+
+INPUT_WIDTH = 28
+INPUT_HEIGHT = 18
+
+
+class PlayerNetwork(nn.Module):
+    def __init__(self):
+        super(PlayerNetwork, self).__init__()
+
+        self.map_layer = nn.Sequential(
+            nn.Conv2d(8, 32, kernel_size=3, stride=1, padding=1),  # 28x18x32
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),  # 14x9x64
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),  # 7х5х64
+            nn.ReLU(),
+            nn.Flatten(),  # 2240
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(INPUT_WIDTH * INPUT_HEIGHT * 32, 512),  # map_layer output + hp + ult possibility
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+        )
+        self.move_head = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 2),
+            nn.Tanh(),
+        )
+        self.shoot_head = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10),
+        )
+        self.ult_head = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, map_inp: Tensor, hp: Tensor, has_ult: Tensor):
+        map_logits = self.map_layer(map_inp)
+        combined = torch.cat([map_logits, hp, has_ult], 1)
+        fc1_out = self.fc1(combined)
+        move = self.move_head(fc1_out)
+        shoot_logits = self.shoot_head(fc1_out)
+        shoot = F.log_softmax(shoot_logits, dim=1)
+
+        if has_ult.any():
+            ult_logit = self.ult_head(fc1_out)
+            ult_logit = ult_logit * has_ult + (1 - has_ult) * (-1e9)
+            ult_prob = torch.sigmoid(ult_logit)
+        else:
+            ult_prob = torch.zeros(fc1_out.size(0), 1, device=fc1_out.device)
+
+        return move, shoot, ult_prob
